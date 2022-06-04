f952683343629fe2a34569250aff296c68c44c1d
Max Coronado Lorca <maxxkcoronado@gmail.com>
2022-06-04 15:31:05 -0400

    add CRG, clear json files utility

---
+++ crg.ps1
@@ -0,0 +1,9 @@
+echo ---------------------------------------------------
+echo 'CRG (Clear Roommates & Gastos)'
+echo 'Reestablecer archivos roommates.json y gastos.json'
+echo ---------------------------------------------------
+echo 'Prosesando ...'
+echo '{"roommates":[]}' > roommates.json
+echo '{"gastos":[]}' > gastos.json
+echo ---------------------------------------------------
+echo 'Listo !!!'
