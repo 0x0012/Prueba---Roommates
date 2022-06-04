5d94c9966bf3e974c136e5d8bc4938dc0574ceb8
Max Coronado Lorca <maxxkcoronado@gmail.com>
2022-06-03 20:47:42 -0400

    add base files

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+ * Prueba - Roommates
+ * @author Max Coronado Lorca
+ */
+
+const http = require('http')
+
+http
+  .createServer((req, res) => {
+  
+  })
+  .listen(3000, () => console.log('✅ SERVER ON : http://localhost:3000/'))
